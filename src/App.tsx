c72fac7f8fee8b6a7781a30274cdb0ee28232f80
yuriiprots <uraprots7849@gmail.com>
2025-11-08 17:58:49 +0200

    minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/app-router";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <AppRouter />
-      </BrowserRouter>
+      <AppRouter />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
 import { BrowserRouter } from "react-router-dom";
-import App from "./app.tsx"
+import App from "./app.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </Provider>
